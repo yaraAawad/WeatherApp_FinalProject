1ea24b015b2cdf736f748077f048ace2a2d9bd8f
yaraAawad <awadyara2000@gmail.com>
2024-02-01 03:51:05 +0300

    semi-final

--- server/server.js
+++ server/server.js
@@ -1,45 +1,58 @@
-const express=require('express')
-const path= require('path')
-const bcrypt = require("bcrypt")
-var bodyParser= require('body-parser')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
 
-const app= express()
-
-
-var urlEncodded= bodyParser.urlencoded({extended:false})
-
-app.use(express.static('public'))
-
-const {MongoClient}= require('mongodb')
+const { MongoClient } = require('mongodb');
 var url = "mongodb+srv://awadyara2000:UTh6s4uvoFVv0fKp@cluster0.lzxhfph.mongodb.net/?retryWrites=true&w=majority";
-
 const client = new MongoClient(url);
-const db= client.db('YarTrainig')
-const coll= db.collection('users')
+const db = client.db('YarTrainig');
+const coll = db.collection('final');
 
-app.set('view engine', 'ejs')
+const urlEncoded = bodyParser.urlencoded({ extended: false });
 
-app.get("/", function(req,res)
-{
-    res.render('login')
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render('login');
 });
 
-app.post("/reg", (req,res)=>
-{
-    res.render('login')
-})
+app.get("/register", (req, res) => {
+    res.render('reg');
+});
 
-app.post("/update", urlEncodded, async(req,res)=>{
-     
-    const result = await coll.updateOne({email:req.body.email, password:req.body.password, name:req.body.name, age:req.body.age, gender:req.body.gender})
+app.get("/login", (req, res) => {
+    res.render('login');
+});
 
-})
+app.post("/login", urlEncoded, async (req, res) => {
+    console.log(req.body.password, req.body.email);
+    const result = await coll.findOne({ email: req.body.email, password: req.body.password });
+    if (result) {
+        res.redirect('/home');
+    } else {
+        res.render('reg');
+    }
+});
 
-var server = app.listen(9001, function()
-{
-    var host= server.address().address
-    var port= server.address().port
-})
+app.post("/reg", urlEncoded, async (req, res) => {
+    console.log(req.body.password, req.body.email);
+    const existingUser = await coll.findOne({ email: req.body.email });
 
+    if (existingUser) {
+        res.render('login');
+    } else {
+        await coll.insertOne({ name: req.body.name, email: req.body.email, password: req.body.password });
+        res.redirect('/home');
+    }
+});
 
-console.log("start server ")
+app.get("/home", (req, res) => {
+    res.render('home');
+});
+
+const port=5000;
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
